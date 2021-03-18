ee7fd05b5bb44cb1facd4b315efddf35424d06da
gaalbence15 <gaalbence15@gmail.com>
2021-03-18 10:03:32 +0100

    Ship movement

--- StarWars.py
+++ StarWars.py
@@ -1,15 +1,52 @@
 import turtle
 
+
+def Up():
+    ypos = ship.ycor()
+    ypos += 20
+    ship.sety(ypos)
+
+
+def Down():
+    ypos = ship.ycor()
+    ypos -= 20
+    ship.sety(ypos)
+
+
+def Right():
+    xpos = ship.xcor()
+    xpos += 20
+    ship.setx(xpos)
+
+
+def Left():
+    xpos = ship.xcor()
+    xpos -= 20
+    ship.setx(xpos)
+
+
 space = turtle.Screen()
 space.setup(width=800, height=600)
 space.bgpic("space.png")
 space.addshape("sprite.gif")
-space.onkeypress(balra, "Left")
+space.listen()
+space.onkeypress(Up, "Up")
+space.onkeypress(Down, "Down")
+space.onkeypress(Right, "Right")
+space.onkeypress(Left, "Left")
 
 
 ship = turtle.Turtle()
 ship.shape("sprite.gif")
+ship.penup()
 
 while True:
-
     space.update()
+    if ship.ycor() > 300:
+        ship.sety(-300)
+    if ship.ycor() < -300:
+        ship.sety(300)
+    if ship.xcor() > 400:
+        ship.setx(-400)
+    if ship.xcor() < -400:
+        ship.setx(400)
